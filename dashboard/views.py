f89752b79d9a4dc07d016f6df4d8370310878514
Bartosz Bednarczyk <bartosz.jan.bednarczyk@gmail.com>
2017-11-05 16:57:06 +0100

    Fixed issue with dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,48 +1,50 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.shortcuts import render
 
 from accounts.models import Account
 from transactions.models import Transaction
 
-# Create your views here.
+
+def get_friends(account):
+    subscribed_groups = account.user.groups.all()
+    friends = []
+    for grp in subscribed_groups:
+        friends += [usr for usr in User.objects.filter(groups__name=grp.name) if
+                    usr.account != account]
+    friends = list(set(friends))  # remove duplicates
+    return friends
 
 
 @login_required(login_url='/login')
 def dash(request):
-    account = Account.objects.get(user=request.user)
+    my_account = Account.objects.get(user=request.user)
     groups = request.user.groups.all()
 
     # A user's "friends" are the other members of the groups that he/she is in.
-    # We may add a more reciprocal system for friends (like Facebook) in the future.
-    friends = set()
+    # We may add a more reciprocal system for friends (like FB) in the future.
+    friends = get_friends(my_account)
 
     expenses_received = 0.0
     expenses_paid = 0.0
-    for g in groups:
-        users = User.objects.filter(groups__name=g.name)
-        for u in users:
-            print(u)
-            friends.add(u)
 
-    transactions_received = Transaction.objects.filter(payee=account)
+    transactions_received = Transaction.objects.filter(payee=my_account)
     for t in transactions_received:
-        if t.status == 'O':
+        if t.status == 'C':
             expenses_received += float(t.amount)
 
-    transactions_paid = Transaction.objects.filter(payer=account)
+    transactions_paid = Transaction.objects.filter(payer=my_account)
     for t in transactions_paid:
         if t.status == 'C':
             expenses_paid += float(t.amount)
 
-    if request.user in friends:
-        friends.remove(request.user)
+    transactions = Transaction.objects.filter(
+        payee=my_account) | Transaction.objects.filter(payer=my_account)
+    transactions = transactions.order_by('-created')[:5]
 
-    transactions = Transaction.objects.filter(payee=account) | Transaction.objects.filter(payer=account)
-    transactions.order_by('-created')[:5]
-
-    return render(request, 'sites/dashboard.html', {'account': account, 'n_groups': groups.count(),
-                                                    'n_friends': len(friends),
-                                                    'expenses_received': "%.2f" % expenses_received,
-                                                    'expenses_paid': "%.2f" % expenses_paid,
-                                                    'transactions': transactions})
+    return render(request, 'sites/dashboard.html',
+                  {'my_account': my_account, 'n_groups': groups.count(),
+                   'n_friends': len(friends),
+                   'expenses_received': "%.2f" % expenses_received,
+                   'expenses_paid': "%.2f" % expenses_paid,
+                   'transactions': transactions})
