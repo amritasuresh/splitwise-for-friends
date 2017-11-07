c6cd7bc4783c829845964509b81247afd03c769e
Amrita Suresh <amritasuresh2209@gmail.com>
2017-11-07 21:11:19 +0100

    Adding documentation to command line interface

--- cli.py
+++ cli.py
@@ -5,10 +5,18 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
 from django import forms
 from datetime import datetime
+import uuid
+from django.contrib.auth.models import Group
 
 from accounts.models import Account
 from transactions.models import Transaction
 from groups.models import UserGroup
+
+#A basic command line interface that lets you create a user, add a transaction, view transactions, create group
+#This tool does not settle the dues like the web interface, we can add that as a later part of the project.
+#Also basic tests like redundancy and false values need to be checked
+
+#This creates a user
 
 def create_user(username, firstname, lastname, password, email):
     user_exists = User.objects.filter(username=username).exists()
@@ -28,6 +36,8 @@ def create_user(username, firstname, lastname, password, email):
         user = authenticate(username=username, password=password)
         Account.objects.create(user=user)
 
+#This creates a transaction within a given group
+
 def add_transaction():
     amount = input("Enter amount\n")
     payer = input("Enter payer username\n")
@@ -46,6 +56,8 @@ def add_transaction():
             acc = Account.objects.get(user_id=useracc.id)
             Transaction.objects.create(name=details, payee=acc, payer=payeracc, amount=amount, group=grp)
 
+#This method is just a placeholder for the user creation to get the details from the command line
+
 def create():
     username = input("Enter user name\n")
     password = input("Enter password\n")
@@ -54,10 +66,14 @@ def create():
     email = input("Enter email\n")
     create_user(username, firstname, lastname, password, email)
 
+#This is the method to view transactions from the database
+
 def view_transactions():
     groupname = input("Enter group name")
     grp = UserGroup.objects.get(group_name=groupname)
     print(Transaction.objects.filter(group=grp))
+
+#This is the method to view the list of all transactions of a given user (the money that the user is owed).
 
 def view_balance():
     username = input("Enter user name")
@@ -65,9 +81,32 @@ def view_balance():
     payeracc = Account.objects.get(user_id=payeruser.id)
     print(Transaction.objects.filter(payer=payeracc))
 
+#This adds existing user to group
 
-from accounts.forms import UserRegistrationForm
-act = input("Hello, pick a choice between the following actions: 1. Create new user 2. Add transaction 3. View transactions 4. View balance\n")
+def add_user_to_group():
+    groupname = input("Enter the group you want to add user to")
+    username = input("Enter user name to be added to the group")
+    grp = UserGroup.objects.get(group_name=groupname)
+    user = User.objects.get(username=username)
+    user.groups.add(grp)
+
+#This creates a new group
+
+def create_new_group():
+    groupname = input("Enter group name")
+    unique_django_group_id = uuid.uuid4()
+
+    while Group.objects.filter(name=unique_django_group_id).exists():
+        unique_django_group_id = uuid.uuid4()
+
+    grp = Group.objects.create(name=unique_django_group_id)
+
+    UserGroup.objects.create(group=grp, group_name=groupname,
+                             created=datetime.now())
+
+#Basic tool to add the details
+
+act = input("Hello, pick a choice between the following actions: 1. Create new user\n 2. Add transaction\n 3. View transactions\n 4. View balance\n 5. Add user to group\n 6. Create new group\n")
 if act == '1':
     create()
 elif act == '2':
@@ -76,7 +115,10 @@ elif act == '3':
     view_transactions()
 elif act == '4':
     view_balance()
-
+elif act == '5':
+    add_user_to_group()
+elif act == '6':
+    create_new_group()
 
 
 
