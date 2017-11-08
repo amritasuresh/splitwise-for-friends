df71e7970c7ceb62cd99c5f1ff6d47fa9867fade
David Tidmarsh <tidmarsh.david@gmail.com>
2017-11-08 02:54:26 +0100

    docstrings for cli

--- cli.py
+++ cli.py
@@ -12,13 +12,20 @@ from accounts.models import Account
 from transactions.models import Transaction
 from groups.models import UserGroup
 
-#A basic command line interface that lets you create a user, add a transaction, view transactions, create group
-#This tool does not settle the dues like the web interface, we can add that as a later part of the project.
-#Also basic tests like redundancy and false values need to be checked
-
-#This creates a user
+# A basic command line interface that lets you create a user, add a transaction, view transactions, create group
+# This tool does not settle the dues like the web interface, we can add that as a later part of the project.
+# Also, basic tests like redundancy and false values need to be checked.
 
 def create_user(username, firstname, lastname, password, email):
+    """
+    This function creates a user.
+    :param username: The user's username
+    :param firstname: The user's first name
+    :param lastname: The user's last name
+    :param password: The user's password
+    :param email: The user's email
+    :return:
+    """
     user_exists = User.objects.filter(username=username).exists()
     email_exists = User.objects.filter(email=email).exists()
 
@@ -36,9 +43,12 @@ def create_user(username, firstname, lastname, password, email):
         user = authenticate(username=username, password=password)
         Account.objects.create(user=user)
 
-#This creates a transaction within a given group
 
 def add_transaction():
+    """
+    This function creates a transaction within a given group by prompting the user information.
+    :return:
+    """
     amount = input("Enter amount\n")
     payer = input("Enter payer username\n")
     group = input("Enter group name\n")
@@ -56,9 +66,12 @@ def add_transaction():
             acc = Account.objects.get(user_id=useracc.id)
             Transaction.objects.create(name=details, payee=acc, payer=payeracc, amount=amount, group=grp)
 
-#This method is just a placeholder for the user creation to get the details from the command line
 
 def create():
+    """
+    This function is a placeholder for the user creation to get the details from the command line.
+    :return:
+    """
     username = input("Enter user name\n")
     password = input("Enter password\n")
     firstname = input("Enter first name\n")
@@ -66,33 +79,45 @@ def create():
     email = input("Enter email\n")
     create_user(username, firstname, lastname, password, email)
 
-#This is the method to view transactions from the database
 
 def view_transactions():
+    """
+    This function allows the user to view transactions from the database.
+    :return:
+    """
     groupname = input("Enter group name")
     grp = UserGroup.objects.get(group_name=groupname)
     print(Transaction.objects.filter(group=grp))
 
-#This is the method to view the list of all transactions of a given user (the money that the user is owed).
 
 def view_balance():
+    """
+    This function displays the list of all transactions for a given user (the money that the user is owed).
+    :return:
+    """
     username = input("Enter user name")
     payeruser = User.objects.get(username=username)
     payeracc = Account.objects.get(user_id=payeruser.id)
     print(Transaction.objects.filter(payer=payeracc))
 
-#This adds existing user to group
 
 def add_user_to_group():
+    """
+    This function adds an existing user to an existing group.
+    :return:
+    """
     groupname = input("Enter the group you want to add user to")
     username = input("Enter user name to be added to the group")
     grp = UserGroup.objects.get(group_name=groupname)
     user = User.objects.get(username=username)
     user.groups.add(grp)
 
-#This creates a new group
 
 def create_new_group():
+    """
+    This function creates a new group.
+    :return:
+    """
     groupname = input("Enter group name")
     unique_django_group_id = uuid.uuid4()
 
@@ -104,7 +129,7 @@ def create_new_group():
     UserGroup.objects.create(group=grp, group_name=groupname,
                              created=datetime.now())
 
-#Basic tool to add the details
+# Basic tool to add the details
 
 act = input("Hello, pick a choice between the following actions: 1. Create new user\n 2. Add transaction\n 3. View transactions\n 4. View balance\n 5. Add user to group\n 6. Create new group\n")
 if act == '1':
