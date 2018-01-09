f227b9781ebd85ee336ebe94917f7bb2d2ec4129
David Tidmarsh <tidmarsh.david@gmail.com>
2018-01-09 18:22:13 +0100

    Update settings.py

--- accounting_for_friends/settings.py
+++ accounting_for_friends/settings.py
@@ -44,13 +44,13 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'accounting_for_friends.urls'
